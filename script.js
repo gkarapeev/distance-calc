ca31ddb4f454698614e6ec7ac06c12ff802bef58
gkarapeev <gkarapeev@gmail.com>
2021-01-11 13:21:16 +0200

    feat: add broken offset to grid center

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MASSIVE TODO: IMPLEMENT SMOETHING DECENT WHICH OFFSETS THE ORIGIN TO THE CENTER OF THE GRID. THE CURRENT SHIT IS RUBBISH.
+
 // Element references
 // Inputs
 const x1_input = document.getElementById("x1");
@@ -6,22 +8,22 @@ const x2_input = document.getElementById("x2");
 const y2_input = document.getElementById("y2");
 
 // Canvas
-const board = document.getElementById("board");
-const canvas = document.getElementById("board");
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // Result
 const lineLength = document.getElementById("line-length");
 
 // Variables
-let lineCount = 30;
+let lineCount = 31;
 let gridSpacing = 20;
 let pointRadius = 5;
+let gridPadding = 40;
 
 let x1 = 3;
 let y1 = 3;
-let x2 = 6;
-let y2 = 6;
+let x2 = 0;
+let y2 = 0;
 
 // Init input values
 x1_input.value = x1;
@@ -57,28 +59,35 @@ function draw() {
     drawLine();
 }
 
-canvas.width = board.offsetWidth;
-canvas.height = board.offsetHeight;
+canvas.width = canvas.offsetWidth + gridPadding * 2;
+canvas.height = canvas.offsetHeight + gridPadding * 2;
 
 draw();
 
 function drawGrid() {
-    ctx.strokeStyle = "#ddd";
 
-    for (let i = 1; i <= lineCount; i++) {
-        let value = i * gridSpacing;
+    for (let i = 1; i <= lineCount - 1; i++) {
+        let start = i * gridSpacing + gridPadding;
+
+        if (i === (lineCount - 1) / 2 + 1) {
+            ctx.strokeStyle = "black";
+        } else {
+            ctx.strokeStyle = "#ddd";
+        }
+
+        const lineLength = canvas.width - gridPadding;
 
         // Horizontal lines
         ctx.beginPath();
-        ctx.moveTo(0, value);
-        ctx.lineTo(1500, value);
+        ctx.moveTo(gridPadding, start);
+        ctx.lineTo(lineLength, start);
         ctx.closePath();
         ctx.stroke();
 
         // Vertical lines
         ctx.beginPath();
-        ctx.moveTo(value, 0);
-        ctx.lineTo(value, 1500);
+        ctx.moveTo(start, gridPadding);
+        ctx.lineTo(start, lineLength);
         ctx.closePath();
         ctx.stroke();
     }
@@ -94,13 +103,13 @@ function drawPoints() {
 
     // Draw point 1
     ctx.beginPath();
-    ctx.arc(gridSpacing * x1, gridSpacing * y1, pointRadius, 0, 360);
+    ctx.arc(gridSpacing * x1 + canvas.width / 2, gridSpacing * y1 + canvas.width / 2, pointRadius, 0, 360);
     ctx.closePath();
     ctx.fill();
 
     // Draw point 2
     ctx.beginPath();
-    ctx.arc(gridSpacing * x2, gridSpacing * y2, pointRadius, 0, 360);
+    ctx.arc(gridSpacing * x2 + canvas.width / 2, gridSpacing * y2 + canvas.width / 2, pointRadius, 0, 360);
     ctx.closePath();
     ctx.fill();
 }
